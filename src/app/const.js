7e49ee3615876c16d10294625d42176d98b29b18
D_arh <dimonarhipon@mail.ru>
2023-10-10 21:49:16 +0300

    fix comment

--- src/app/const.js
+++ src/app/const.js
@@ -5,7 +5,7 @@ export const TabNames = {
 };
 
 export const Regular = {
-	// Number: /^\d{4}\d{4}\d{4}\d{4}$/,
+	Number: /^\d{4} \d{4} \d{4} \d{4}$/,
 	Cvv: /^\d{3}$/,
 	Name: /^[a-zA-Z\s]*$/,
 	Expiry: /^(0[1-9]|1[0-2])\/?([0-9]{2})$/,
